a178771913ba919faae03550825dd62337827dff
Tangworld <948138224@qq.com>
2017-06-30 14:49:26 +0800

    2017063002

--- locator/views.py
+++ locator/views.py
@@ -1,4 +1,6 @@
+#-*- coding:utf-8 -*-
 from django.shortcuts import render
+from locator import models
 
 # Create your views here.
 
@@ -6,4 +8,35 @@ def index(request):
     return render(request, 'login.html', )
 
 def login(request):
-    return render(request, 'index.html')
+    validate = {}
+    v1 = ""
+    v2 = ""
+    flag = True
+    validatepassword = ""
+
+    if request.method == "POST":
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        try:
+            validatepassword = models.User.objects.get(username=username)
+            if validatepassword.password != password:
+                v2 = "密码错误！"
+                flag = False
+            print validatepassword
+        except Exception, e:
+            v2 = "密码错误！"
+            flag = False
+
+        if username == "":
+            v1 = "用户名不能为空！"
+            flag = False
+        if password == "":
+            v2 = "密码不能为空！"
+            flag = False
+        validate = {"v1": v1, "v2": v2}
+
+
+    if flag:
+        return render(request, 'index.html')
+    else:
+        return render(request, 'login.html', {"validate": validate})
