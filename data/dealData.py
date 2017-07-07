785539af8332ce8b4b01de981a67a801ee00b02a
Tangworld <948138224@qq.com>
2017-07-07 09:11:40 +0800

    合并

--- data/dealData.py
+++ data/dealData.py
@@ -1,28 +1,34 @@
+# -*- coding: UTF-8 -*-
 import MySQLdb
 
 def main():
     input = open("aspectj.txt")
     lines = input.readlines()
-    db = MySQLdb.connect("localhost", "root", "root", "CodeDefectLocation")
+    db = MySQLdb.connect("localhost", "root", "root", "locator")
     cursor = db.cursor()
 
+    cnt = 0
 
     for line in lines:
         temp = eval(line)
-        priority = temp['pr']
-        opendate = temp['tm']
+        priority = str(temp['pr'])
+        opendate = str(temp['tm'])
         reporter = temp['rp']
-        component = temp['cm']
-        tr = temp['tr']
-        fileloc = temp['ts']
-        severity = temp['sv']
-        version = temp['vs']
-        keywords = temp['ws']
-        platform = temp['pf']
-        os = temp['os']
-        bugid = temp['id']
-        cursor.execute("INSERT INTO Report(bugid,reporter,priority,opendate,component,) VALUES('Jack London')")
+        component = str(temp['cm'])
+        tr = str(temp['tr'])
+        fileloc = str(temp['ts'])
+        print reporter
+        severity = str(temp['sv'])
+        version = str(temp['vs'])
+        keywords = str(temp['ws'])
+        platform = str(temp['pf'])
+        os = str(temp['os'])
+        bugid = str(temp['id'])
+        #cursor.execute("INSERT INTO Report(reporter,opendate,fileloc,component,version,platform,os,priority,severity,bugid,keywords)"
+        #               " VALUES("+reporter+","+opendate+","+fileloc+","+component+","+version+","+platform+","+os+","+priority+","+severity+","+bugid+","+keywords+")")
         print bugid
+        cnt += 1
+    print cnt
 
 if __name__ == "__main__":
     main()
