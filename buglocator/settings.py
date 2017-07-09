e8d1907127a9001904eddedda0afeeada8faed04
Tangworld <948138224@qq.com>
2017-07-09 17:14:30 +0800

    2017070906

--- buglocator/settings.py
+++ buglocator/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'locator',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
