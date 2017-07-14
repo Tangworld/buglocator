95643a138e1a41f812aaef4f9ed904b9c3a295db
Zhangtd <zhangtd95@outlook.com>
2017-07-14 15:10:43 +0800

    管理员主页的时间轴

--- locator/views.py
+++ locator/views.py
@@ -64,7 +64,43 @@ def login(request):
         request.session['avatarloc'] = validatepassword.avatarloc
 
         if validatepassword.isadmin == 'yes':
-            return render(request, 'admin/index_admin.html')
+            members = []
+            dis_reports = []
+            try:
+                productobj = models.ProUser.objects.get(user_id=validatepassword.id)
+                productId = int(productobj.product_id)
+                userids = models.ProUser.objects.filter(product_id=productId)
+
+                reports = models.Report.objects.all()
+                for report in reports:
+                    time_local = time.localtime(float(report.opendate))
+                    dt = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
+                    if report.status == 'fixed':
+                        tmp = [dt, '2', report.bugid, report.summary, report.assignee]
+                        dis_reports.append(tmp)
+                    elif report.status == 'unfixed':
+                        tmp = [dt, '3', report.bugid, report.summary, report.assignee]
+                        dis_reports.append(tmp)
+                for report in reports:
+                    time_local = time.localtime(float(report.opendate))
+                    dt = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
+                    tmp = [dt, '1', report.bugid, report.summary, report.reporter]
+
+                dis_reports.sort(lambda x, y: cmp(x[0], y[0]))
+                dis_content = []
+                for i in range(len(dis_reports) - 1, len(dis_reports) - 16, -1):
+                    if i >= 0:
+                        dis_content.append(dis_reports[i])
+                    else:
+                        break
+
+                for userid in userids:
+                    member = models.User.objects.get(id=userid.user_id)
+                    members.append(member)
+            except Exception, e:
+                print e
+            print dis_content
+            return render(request, 'admin/index_admin.html', {'members': members, 'dis_reports': dis_content})
         else:
             members = []
             dis_reports = []
