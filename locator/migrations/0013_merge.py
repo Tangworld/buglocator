8b5c07a825ce41f1cf6a642e03f44a35b7d99d52
Tangworld <948138224@qq.com>
2017-08-04 15:49:58 +0800

    merge

---
+++ locator/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('locator', '0011_filemap_path_l2ss'),
+        ('locator', '0012_auto_20170803_0650'),
+    ]
+
+    operations = [
+    ]
